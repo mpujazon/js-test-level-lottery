cbb4353d74f67d1258275acc3fdc29fd94e93500
Miguel Pujazón <mpujazoncardenas@gmail.com>
2025-12-16 13:16:52 +0100

    handleSubmit Function now is managing getResult()

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+import { getResult } from "./src/scripts/logic.js";
+
 let result = {};
 let resultsArr = [];
 
 const handleSubmit = (e) => {
+    e.preventDefault();
+    try{
+        result = getResult(Number(e.target[0].value));
+        console.log(result);
+        
 
+    }catch(err){
+        console.error(err);
+    }
 }
 const form = document.getElementById('lottery-form');
 form.addEventListener('submit', handleSubmit);
--- src/scripts/logic.js
+++ src/scripts/logic.js
@@ -1,5 +1,5 @@
 export const getResult = (userNumber) => {
-    if(!(userNumber instanceof Number)) throw new Error ('userNumber must be a number type.');
+    if(!userNumber instanceof Number) throw new Error ('userNumber must be a number type.');
     const randomNumber = getRandomNumber();
     return {
         userNum: userNumber,
